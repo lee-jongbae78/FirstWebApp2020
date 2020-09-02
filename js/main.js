e5ab8c4e459e470ea3c7a4cfe32a6e5f2f66bb84
lee-jongbae78 <leejongbae78@gmail.com>
2020-09-02 14:02:03 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,14 @@
 const name ="ljb";
-//console.log(name);
-
-const days = [ "mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-//console.log(days[2]);
 
 const ljbInfo = {name:"leejongbae", age:43, kids : ["first", "second", "third"]}; //array, object 혼합
-//console.log(ljbInfo.kids[2]);
+
+document.write("테스트");
 
 function sayHello(a){
     return `${a} hi!!!`;
 }
 
 const greetLjb = sayHello("LeeJB");
-
-const calculator = {
-    plus: function(a, b){
-        return a + b;
-    },
-    minus: function(a, b){
-        return a - b;
-    }
-}
-
-const _plus = calculator.plus(10, 5);
-const _minus = calculator.minus(20, 3);
 
 const title = document.querySelector("#title");
 console.log(title);
