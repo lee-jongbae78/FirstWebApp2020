9a45448ac4dfa5112a74102821d9e01b9b41a6d8
lee-jongbae78 <leejongbae78@gmail.com>
2020-09-29 15:02:18 +0900

    body changes

--- js/main.js
+++ js/main.js
@@ -18,8 +18,18 @@ title.innerHTML = name + "님 학습을 시작합니다.";
 title.style.color = "red";
 //document.title= "title changed!";
 
-const changeBgToBlack = document.querySelector("body");
-
 function changeBgB() {
-  changeBgToBlack.style.backgroundColor = "black";
+  const target = document.querySelector("body");
+  const target = document.querySelector("body");
+
+  if (this.value === "day") {
+    target.style.backgroundColor = "black";
+
+    this.value = "night";
+    console.log(target.value);
+  } else {
+    target.style.backgroundColor = "white";
+    this.value = "day";
+    console.log(target.value);
+  }
 }
