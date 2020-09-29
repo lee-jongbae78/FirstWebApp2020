368258fe5b06184bd4e74345aa6e486e68cdaada
lee-jongbae78 <leejongbae78@gmail.com>
2020-09-29 13:41:09 +0900

    javascript study1

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
-const name ="ljb";
+const name = "ljb";
 
-const ljbInfo = {name:"leejongbae", age:43, kids : ["first", "second", "third"]}; //array, object 혼합
+const ljbInfo = {
+  name: "leejongbae",
+  age: 43,
+  kids: ["first", "second", "third"],
+}; //array, object 혼합
 
-function sayHello(a){
-    return `${a} hi!!!`;
+function sayHello(a) {
+  return `${a} hi!!!`;
 }
 
 const greetLjb = sayHello("LeeJB");
 
 const title = document.querySelector("#title");
 console.log(title);
-title.innerHTML = name +"님 학습을 시작합니다.";
+title.innerHTML = name + "님 학습을 시작합니다.";
 title.style.color = "red";
 //document.title= "title changed!";
 
+const changeBgToBlack = document.querySelector("body");
+
+function changeBgB() {
+  changeBgToBlack.style.backgroundColor = "black";
+}
